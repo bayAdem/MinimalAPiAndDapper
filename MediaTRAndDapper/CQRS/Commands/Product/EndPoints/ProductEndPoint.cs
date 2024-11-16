0bed7525e35578a364371cd753e9a149ac27b01f
Adem Bayram <bayadem1903@gmail.com>
2024-11-16 08:58:08 +0300

    Adem
    düzenleme

--- MediaTRAndDapper/CQRS/Commands/Product/EndPoints/ProductEndPoint.cs
+++ MediaTRAndDapper/CQRS/Commands/Product/EndPoints/ProductEndPoint.cs
@@ -10,7 +10,7 @@ namespace MediaTRAndDapper.CQRS.Commands.Product.EndPoints
 
         public override void Configure()
         {
-            Post("/create-Product");
+            Post("/AddProduct");
             AllowAnonymous();
         }
 
